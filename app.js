901ed6e13a6d533a831ed1d04169e9a4e5182a78
EmmyCodes1106 <emmanuelfadayomi1106@gmail.com>
2024-07-25 17:15:30 +0100

    JavaScript

---
+++ app.js
@@ -0,0 +1,50 @@
+const cards = [
+    {
+        name: "card1",
+        img: "./Assets/img1.jpg",
+        number: 1,
+    },
+    {
+        name: "card2",
+        img: "./Assets/img2.jpg",
+        number: 2,
+    },
+    {
+        name: "card3",
+        img: "./Assets/img3.jpg",
+        number: 3,
+    },
+    {
+        name: "card4",
+        img: "./Assets/img4.jpg",
+        number: 4,
+    },
+    {
+        name: "card5",
+        img: "./Assets/img5.jpg",
+        number: 5,
+    },
+    {
+        name: "card6",
+        img: "./Assets/img6.jpg",
+        number: 6,
+    },
+    {
+        name: "card7",
+        img: "./Assets/img7.jpg",
+        number: 7,
+    },
+    {
+        name: "card8",
+        img: "./Assets/img8.jpg",
+        number: 8,
+    },
+]
+const player1Canvas = document.getElementById("player1-canvas");
+const player2Canvas = document.getElementById("player2-canvas");
+
+
+function init() {
+
+}
+init();
